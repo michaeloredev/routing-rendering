d17d00b6f4872098df1859e0464e32a507386c22
more0210 <michael.ore.dev@gmail.com>
2024-10-08 22:15:58 -0400

    parallel routes

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { DUMMY_NEWS } from '@/dummy-news'
+import { notFound } from 'next/navigation'
 
 export default function page({ params }) {
     const newsSlug = params.slug
     const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug)
+
+    if (!newsItem) {
+      notFound();
+    }
+
   return (
     <article className='news-article'>
       <header>
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found</h1>
+      <p>The page cannot be reached.</p>
+    </div>
+  );
+}
